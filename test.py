97d26cc1b680dc2b5c13f61787c737e3267aa739
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-01-22 19:49:53 +0000

    test passing for fizz

---
+++ test.py
@@ -0,0 +1,13 @@
+# multiple of 3 -> fizz
+# multiple pf 5 -> buzz
+# multiples of both -> fizzbuzz
+# number
+
+import pytest
+
+def fizzbuzz(num):
+    return("fizz")
+
+
+def test_fizzbuzz():
+    assert fizzbuzz(3) == "fizz"
