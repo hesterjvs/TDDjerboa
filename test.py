ae5cf03af0b3e1c870a571be86c25cf7bd6844c9
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-01-30 11:00:33 +0000

    test: fizzbuzz range

--- test.py
+++ test.py
@@ -6,22 +6,19 @@
 import pytest
 import sys
 
-
 def fizzbuzz(num):
-  #  for number in num:
-        if num%3 == 0 and num%5 == 0 :
-            return("fizzbuzz")
-        elif num%3 == 0:
-            return("fizz")
-        elif num%5 == 0:
-            return("buzz")
-        else:
-            return(num)
+    if num % 3 == 0 and num % 5 == 0 :
+        return("fizzbuzz")
+    elif num % 3 == 0:
+        return("fizz")
+    elif num % 5 == 0:
+        return("buzz")
+    else:
+        return(str(num))
 
-def fizzbuzz_range(nn, out=sys.stdout):
-    for num in (1,nn+1):
-        out.write(fizzbuzz(num) + "\n")
-
+def fizzbuzz_range(nn):
+    for num in range(1, nn+1):
+        print(str(fizzbuzz(num)))
 
 def test_fizzbuzz():
     assert fizzbuzz(3) == "fizz"
@@ -33,9 +30,8 @@ def test_fizzbuzz():
     assert fizzbuzz(15) == "fizzbuzz"
 
 def test_fizzbuzz():
-    assert fizzbuzz(8) == 8
+    assert fizzbuzz(8) == "8"
 
-fizzbuzz_range(10)
-
-#def test_fizzbuzz_range():
-  #  assert fizzbuzz_range(10) == 1, 2, "fizz" 4 "buzz", 6, 7, 8, "fizz", "buzz"
+def test_fizzbuzz_range():
+    value = fizzbuzz_range(10)
+    assert value == print("1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz")
